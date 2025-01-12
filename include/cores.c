68b48372d2433b482b5a5ea2f408d60e17a1cd1e
machadof90 <valterig@hotmail.com>
2025-01-12 02:18:53 -0300

    foramtando

--- include/cores.c
+++ include/cores.c
@@ -11,6 +11,34 @@
 #define BUTTON_B 6
 #define BUTTON_J 22
 
+void iniciaComponentes(){
+//iniciando LEDs
+gpio_init(LED_B);
+gpio_set_dir(LED_B, GPIO_OUT);
+gpio_init(LED_R);
+gpio_set_dir(LED_R, GPIO_OUT);
+gpio_init(LED_G);
+gpio_set_dir(LED_G, GPIO_OUT);
+//inciando buzzers
+gpio_init(BUZZER_A);
+gpio_set_dir(BUZZER_A, GPIO_OUT);
+gpio_init(BUZZER_B);
+gpio_set_dir(BUZZER_B, GPIO_OUT);
+//iniciando botoes
+gpio_init(BUTTON_A);
+gpio_set_dir(BUTTON_A, GPIO_IN);
+gpio_pull_up(BUTTON_A);
+gpio_init(BUTTON_B);
+gpio_set_dir(BUTTON_B, GPIO_IN);
+gpio_pull_up(BUTTON_B);
+gpio_init(BUTTON_J);
+gpio_set_dir(BUTTON_J, GPIO_IN);
+gpio_pull_up(BUTTON_J);
+}
+
+
+
+
 void luzInicial(){
 vermelho();
 sleep_ms(200);
--- memoria.c
+++ memoria.c
@@ -17,32 +17,6 @@ int tamanho = 0;
 char segredo[10];
 char botao;
 bool acerto;
-
-void iniciaComponentes(){
-//iniciando LEDs
-gpio_init(LED_B);
-gpio_set_dir(LED_B, GPIO_OUT);
-gpio_init(LED_R);
-gpio_set_dir(LED_R, GPIO_OUT);
-gpio_init(LED_G);
-gpio_set_dir(LED_G, GPIO_OUT);
-//inciando buzzers
-gpio_init(BUZZER_A);
-gpio_set_dir(BUZZER_A, GPIO_OUT);
-gpio_init(BUZZER_B);
-gpio_set_dir(BUZZER_B, GPIO_OUT);
-//iniciando botoes
-gpio_init(BUTTON_A);
-gpio_set_dir(BUTTON_A, GPIO_IN);
-gpio_pull_up(BUTTON_A);
-gpio_init(BUTTON_B);
-gpio_set_dir(BUTTON_B, GPIO_IN);
-gpio_pull_up(BUTTON_B);
-gpio_init(BUTTON_J);
-gpio_set_dir(BUTTON_J, GPIO_IN);
-gpio_pull_up(BUTTON_J);
-}
-
 void enigma(){
 srand(time(NULL));
 for(int i = 0;i < tamanho; i++){
@@ -95,6 +69,7 @@ gpio_put(LED_G, 0);
 int main() {
 stdio_init_all();
 iniciaComponentes();
+
 while (true) {
     tamanho = tamanho + 1;
     enigma();
